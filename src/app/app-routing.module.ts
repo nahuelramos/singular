0a9aca8d45122623a1c6ab87cd74e25505a01427
Nahuel Ramos <nahuelramos.nr@gmail.com>
2019-03-10 12:48:10 +0100

    Nahuel - page-not-found - add core module and page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PageNotFoundComponent } from 'src/app/core';
+
+const routes: Routes = [  
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
